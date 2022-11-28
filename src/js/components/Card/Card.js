987a5b01aab23d08006ca739f84ddffe47ae02f7
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2022-11-27 20:01:03 -0500

    added lazy image loading and removed unused packages

--- src/js/components/Card/Card.js
+++ src/js/components/Card/Card.js
@@ -1,9 +1,10 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import "./Card.scss";
 
 export default function Card({ children, img, onClick, className }) {
   return (
     <div className={`${className || ""} card`} onClick={onClick}>
-      <img src={`${img}`} alt={`${img}`} className="card-bg" />
+      <LazyLoadImage src={`${img}`} alt={`${img}`} className="card-bg" />
       <div className="card-bg-mask" />
       <div className="card-content">{children}</div>
     </div>
