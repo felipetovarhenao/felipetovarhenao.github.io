21576390b5b6a4999afa3815628a99a86d00b5b1
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-02-14 16:06:20 -0500

    separating cv publications into categories

--- src/js/views/CvView/PublicationsSubView/PublicationsSubView.js
+++ src/js/views/CvView/PublicationsSubView/PublicationsSubView.js
@@ -4,23 +4,51 @@ import { Icon } from "@iconify/react";
 export default function PublicationsSubView({ publications }) {
   return (
     <div className="publications-subview">
-      {publications
-        .sort((a, b) => b.date - a.date)
-        .map((publication, i) => (
-          <div className="publication" key={i}>
-            <span className="author">{publication.author} </span>
-            <span className="date"> ({publication.date}). </span>
-            <a className="name" href={publication.doi} target="_blank" rel="noreferrer">
-              {publication.name}
-            </a>
-            <span className="publisher">. {publication.publisher}, </span>
-            <span className="edition">{publication.edition}. </span>
-            <span className="pages">{`pp. ${publication.pages[0]}-${publication.pages[1]}`}. </span>
-            <a className="doi" href={publication.doi} target="_blank" rel="noreferrer">
-              <Icon className="icon" icon="mdi:link-variant" />
-            </a>
+      {Object.keys(publications).map((pubCategory, n) => {
+        const pubs = publications[pubCategory];
+        return (
+          <div className="publications" key={n}>
+            <div className="header">{pubCategory}</div>
+            {pubs
+              .sort((a, b) => (b.date || b.year) - (b.date || a.year))
+              .map((publication, i) => (
+                <div className="publication" key={i}>
+                  {pubCategory !== "recordings" ? (
+                    <>
+                      <span className="author">{publication.author} </span>
+                      <span className="date"> ({publication.date}). </span>
+                      <a className="name" href={publication.doi} target="_blank" rel="noreferrer">
+                        {publication.name}
+                      </a>
+                      <span className="publisher">. {publication.publisher}, </span>
+                      <span className="edition">{publication.edition}. </span>
+                      <span className="pages">{`pp. ${publication.pages[0]}-${publication.pages[1]}`}. </span>
+                    </>
+                  ) : (
+                    <>
+                      <span className="album">{publication.album}</span>
+                      <span className="date"> ({publication.year}). </span>
+                      <a className="name" href={publication.url} target="_blank" rel="noreferrer">
+                        {publication.track}
+                      </a>
+                      <span className="recordLabel">. {publication.recordLabel}. </span>
+                      <span className="performers">
+                        {publication.performers.map((performer, m) => (
+                          <span key={m}>
+                            {performer.name} ({performer.role}){m === publication.performers.length - 1 ? "." : ", "}
+                          </span>
+                        ))}{" "}
+                      </span>
+                    </>
+                  )}
+                  <a className="doi" href={publication.doi || publication.url} target="_blank" rel="noreferrer">
+                    <Icon className="icon" icon="mdi:link-variant" />
+                  </a>
+                </div>
+              ))}
           </div>
-        ))}
+        );
+      })}
     </div>
   );
 }
