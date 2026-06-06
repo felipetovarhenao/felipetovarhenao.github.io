955a9207aee4d81eefd274823f24b3e949a7f0f5
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2026-06-06 11:12:16 -0400

    update CV

--- src/js/components/ShaderToy/ShaderToy.js
+++ src/js/components/ShaderToy/ShaderToy.js
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import "./ShaderToy.scss";
 
-export default function ShaderToy({ id, className = "", gui = false, paused = true, muted = true, t = 0 }) {
+export default function ShaderToy({ id, className = "", gui = true, paused = true, muted = true, t = 0 }) {
   const [clicked, setClicked] = useState(false);
   return (
     <div className={`${className} shadertoy ${clicked ? "clicked" : ""}`} onClick={() => setClicked(true)}>
-      <iframe
-        title={id}
-        className="inner"
-        frameBorder="0"
-        src={`https://www.shadertoy.com/embed/${id}?gui=${gui}&t=${t}&paused=${paused}&muted=${muted}`}
-        allowFullScreen
-      />
+      <img alt="hi" title={id} className="inner" src={`https://www.shadertoy.com/media/shaders/XfKfWd.jpg`} />
     </div>
   );
 }
